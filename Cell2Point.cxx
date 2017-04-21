1eed647a05beb8ed78cb13f35aff3fb8a2701c4f
Rashed Karim <rk10@isd.kcl.ac.uk>
2017-04-21 12:25:03 +0100

    Code cleanup

--- Cell2Point.cxx
+++ Cell2Point.cxx
@@ -1,29 +1,28 @@
 #define HAS_VTK 1
 #include <vtkPointData.h>
 #include <vtkPolyData.h>
-#include <vtkFloatArray.h>
 #include <vtkSmartPointer.h>
 #include <vtkPolyDataReader.h>
 #include <vtkPolyDataWriter.h>
 #include "vtkCellDataToPointData.h"
 #include "vtkFileOutputWindow.h"
 
-#define MAXDIST 50.0
 
 int main(int argc, char * argv[])
 {
-	// Input file
-
+	
+	// Dirty check arguments 
 	if (argc < 3)
 	{
 		cout << "Usage: <in_vtk> <out_vtk>" << endl;
 		exit(1); 
 	}
 
-	char * input_file = argv[1];
+	// read command-line arguments 
+	char* input_file = argv[1];
 	char* output_file = argv[2]; 
 
-
+	// For error logging, check your folder for vtkLog.txt for VTK run-time error messages 
 	vtkSmartPointer<vtkFileOutputWindow> fileOutputWindow = vtkSmartPointer<vtkFileOutputWindow>::New();
 	fileOutputWindow->SetFileName("vtkLog.txt");
 
@@ -33,21 +32,26 @@ int main(int argc, char * argv[])
 		outputWindow->SetInstance(fileOutputWindow);
 	}
 
+	// Now read the VTK file 
 	vtkPolyDataReader * reader = vtkPolyDataReader::New();
 	reader->SetFileName(input_file);
 	vtkPolyData* poly = vtkPolyData::New();
 	reader->Update();
 	poly = reader->GetOutput();
 
+	// Conversion to point data 
 	vtkCellDataToPointData* c2p = vtkCellDataToPointData::New();
 	c2p->SetInputData(poly); 
 	c2p->Update(); 
 	cout << "Cell data to point data error code = " << c2p->GetErrorCode() << endl;
 
+	// Write to a file 
 	vtkPolyDataWriter* writer = vtkPolyDataWriter::New();
 	writer->SetInputData(c2p->GetOutput());
 	writer->SetFileName(output_file);
 	writer->Update(); 
+
+	// Complete! 
 	
 
 }
